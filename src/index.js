1db095e6312b81d90808fed47fa7d4031c9d5791
seema ughade <105380178+Seema-Ughade@users.noreply.github.com>
2024-07-18 23:28:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import express from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
-const hostname = process.env.HOSTNAME;
-const port = process.env.PORT || 4000;
+const hostname = 'localhost'
+const port = 4000;
 
 app.use(cors());
 
