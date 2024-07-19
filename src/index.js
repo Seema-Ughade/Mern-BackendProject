4c05ee4819eb7aaeef0c8491d03497d324f3d42b
seema <seemarughade1363@gmail.com>
2024-07-19 11:55:05 +0530

    all data updated

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); 
+
 const app = express();
-const hostname = process.env.HOSTNAME;
-const port = process.env.PORT || 4000;
+const hostname = process.env.HOST || 'localhost'; 
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-//home page
-app.get('/courses', (req , res)=>{
-    const courses = [{
-        id : 1,
-        courseName: "MERN"
-    },
-    {
-        id : 2,
-        courseName: "MEAN"
-    },
-    {
-        id : 3,
-        courseName: "MERP"
-    },
-
-   ]
+// Home page
+app.get('/courses', (req, res) => {
+    const courses = [
+        {
+            id: 1,
+            courseName: "MERN"
+        },
+        {
+            id: 2,
+            courseName: "MEAN"
+        },
+        {
+            id: 3,
+            courseName: "MERP"
+        }
+    ];
     res.send(courses);
 });
 
-//services page
-app.get('/services',(req , res)=>{
+// Services page
+app.get('/services', (req, res) => {
+    res.send('Hello from services page');
+});
 
-    res.send('Hello form services page');
-
-})
-
-app.listen(port, hostname, () =>{
-    console.log(`server is running on http://${hostname}:${port}`)
+app.listen(port, hostname, () => {
+    console.log(`Server is running on http://${hostname}:${port}`);
 });
