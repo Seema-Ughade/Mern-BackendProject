1a903ade84cd1ef53e1f700c010424ea53d995b9
seema <seemarughade1363@gmail.com>
2024-07-19 13:24:15 +0530

    new add

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 const hostname = process.env.HOSTNAME || 'localhost'; 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 
